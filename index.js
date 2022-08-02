0f79f09de19536c5eb01f96f52615ffd14ab9b55
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-02 19:04:05 -0300

    feat: Cria rota get para o endpoint /talker

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const talkersFile = './talker.json';
+
+// const talkers = fs.readFile(talkersFile, 'utf-8', (err, data) => {
+//   if (err) {
+//     console.error(`Não foi possível ler o arquivo ${talkersFile}\n Erro: ${err}`);
+//     process.exit(1);
+//   }
+//   return data;
+// });
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,6 +21,19 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (_req, res) => {
+  const talkers = await fs.readFile(talkersFile);
+  const talkersParsed = await JSON.parse(talkers);
+  // const data = [];
+  // talkersParsed.forEach((talker) => data.push(talker));
+  // return res.status(200).json(data);
+
+  if (talkersParsed.length === 0) {
+    return res.status(200).json([]);
+  }
+  return res.status(200).json(talkersParsed);
 });
 
 app.listen(PORT, () => {
