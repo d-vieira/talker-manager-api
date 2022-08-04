5f883c3a3b44ae63c74e95b9f02ef6d57c5ebbba
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-04 11:07:29 -0300

    feat: Cria funcoes helpers para leitura e escrita em arquivos

--- helpers/index.js
+++ helpers/index.js
@@ -1,7 +1,21 @@
+const fs = require('fs').promises;
 const crypto = require('crypto');
 
 const generateToken = () => crypto.randomBytes(8).toString('hex');
 
+const readFile = async (path) => {
+  const rawFile = await fs.readFile(path);
+  const parsedFile = await JSON.parse(rawFile);
+  return parsedFile;
+};
+
+const writeFile = async (path, content) => {
+  const toString = JSON.stringify(content);
+  await fs.writeFile(path, toString);
+};
+
 module.exports = {
   generateToken,
+  readFile,
+  writeFile,
 };
